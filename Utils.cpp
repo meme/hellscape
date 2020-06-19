b746e8f02bc65da94e71d00ed90b5f4b4ec130e2
liufengwenyu <liufengwenyu@163.com>
2020-06-19 10:32:03 +0800

    keep consistent with the style of the code-base

--- Utils.cpp
+++ Utils.cpp
@@ -6,50 +6,45 @@ using namespace std;
 
 bool toObfuscate(bool flag, function *f, string attribute)
 {
-    /* skip declaration*/
-    //return false;
+  /* skip declaration*/
+  //return false;
 
-    /*skip external linkage*/
-    //return false;
+  /*skip external linkage*/
+  //return false;
 
-    /*skip some functions that cannot currently be handled */
-    //return false;
+  /*skip some functions that cannot currently be handled */
+  //return false;
 
-    /*check attribute*/
-    std::string attr = attribute;
-    std::string attrNo = "no" + attr;
+  /*check attribute*/
+  std::string attr = attribute;
+  std::string attrNo = "no" + attr;
 
-    tree attri = DECL_ATTRIBUTES(f->decl);
-    if(attri)
+  tree attri = DECL_ATTRIBUTES(f->decl);
+  if(attri)
+  {
+    tree q = lookup_attribute("obfus", attri);
+    if(q)
     {
-        tree q = lookup_attribute("obfus", attri);
-        if(q)
+      tree qq = q;
+      while(qq)
+      {
+        std::string name = TREE_STRING_POINTER (TREE_VALUE(TREE_VALUE(qq)));
+        if(!name.empty())
         {
-            tree qq = q;
-            while(qq)
-            {
-                std::string name = TREE_STRING_POINTER (TREE_VALUE(TREE_VALUE(qq)));
-                if(!name.empty())
-                {
-                    if(name.find(attrNo) != string::npos)
-                    {
-                        std::cerr << "in " << function_name(f) <<" find " << name << "\n";
-                        return false;
-                    }
+          if(name.find(attrNo) != string::npos)
+          {
+            return false;
+          }
 
-                    if(name.find(attr) != string::npos)
-                    {
-                        std::cerr << "in " << function_name(f) <<" find " << name << "\n";
-                        return true;
-                    }
-                    
-                }
-                qq = TREE_CHAIN (qq);
-            }
+          if(name.find(attr) != string::npos)
+          {
+            return true;
+          }
+            
         }
+        qq = TREE_CHAIN (qq);
+      }
     }
-    if (flag == true) {
-        return true;
-    } 
-    return false;
+  }
+  return flag;
 }
