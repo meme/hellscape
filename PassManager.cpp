c5b66e510c499f94e8d0166c4fd11b29f941de1f
meme <meme@users.noreply.github.com>
2020-06-20 09:34:28 -0400

    Clean up styling

--- PassManager.cpp
+++ PassManager.cpp
@@ -38,29 +38,12 @@ static struct plugin_info my_plugin_info = {"1.0.0",
 
 
 static tree
-handle_obfus_attribute (tree *node, tree name, tree args, int flags, bool *no_add_attrs)
-{
+handle_obfus_attribute (tree *node, tree name, tree args, int flags, bool *no_add_attrs) {
   return NULL_TREE;
 }
 
-/* Attribute definition */
-
 static struct attribute_spec fla_attr =
-{ "obfus", 1, 1, false,  false, false, false, handle_obfus_attribute, NULL};
-
-
-/* Plugin callback called during attribute registration */
-
-static void 
-register_attributes (void *event_data, void *data) 
-{
-  register_attribute (&fla_attr);
-}
-
-void finish_gcc(void* gcc_data, void* user_data) {
-  // Delete the RNG.
-  delete (Random*) user_data;
-}
+  { "obfus", 1, 1, false,  false, false, false, handle_obfus_attribute, NULL};
 
 int plugin_init(struct plugin_name_args* plugin_info,
                 struct plugin_gcc_version* version) {
@@ -174,7 +157,9 @@ seed_error:
   viz_pass_info.ref_pass_instance_number = 1;
   viz_pass_info.pos_op = PASS_POS_INSERT_AFTER;
 
-  register_callback (plugin_info->base_name, PLUGIN_ATTRIBUTES, register_attributes, NULL);
+  register_callback (plugin_info->base_name, PLUGIN_ATTRIBUTES, [](void* event_data, void* user_data) {
+    register_attribute(&fla_attr);
+  }, NULL);
   
   register_callback(plugin_info->base_name, PLUGIN_PASS_MANAGER_SETUP, nullptr,
                     &sub_pass_info);
@@ -185,7 +170,9 @@ seed_error:
   register_callback(plugin_info->base_name, PLUGIN_PASS_MANAGER_SETUP, nullptr,
                     &viz_pass_info);
 
-  register_callback(plugin_info->base_name, PLUGIN_FINISH, finish_gcc, random);
+  register_callback(plugin_info->base_name, PLUGIN_FINISH, [](void* event_data, void* user_data) {
+    delete (Random*) user_data;
+  }, random);
 
   return 0;
 }
--- Utils.cpp
+++ Utils.cpp
@@ -2,49 +2,46 @@
 #include <stringpool.h>
 #include <attribs.h>
 
-using namespace std;
+#include <string>
 
-bool toObfuscate(bool flag, function *f, string attribute)
-{
-  /* skip declaration*/
-  //return false;
+using std::string;
 
-  /*skip external linkage*/
-  //return false;
+bool toObfuscate(bool flag, function* f, string attribute) {
+  /* skip declaration */
+  // return false;
 
-  /*skip some functions that cannot currently be handled */
-  //return false;
+  /* skip external linkage */
+  // return false;
 
-  /*check attribute*/
-  std::string attr = attribute;
-  std::string attrNo = "no" + attr;
+  /* skip some functions that cannot currently be handled */
+  // return false;
+
+  // Check the attribute
+  string attr = attribute;
+  string attrNo = "no" + attr;
 
   tree attri = DECL_ATTRIBUTES(f->decl);
-  if(attri)
-  {
+  if (attri) {
     tree q = lookup_attribute("obfus", attri);
-    if(q)
-    {
+    if (q) {
       tree qq = q;
-      while(qq)
-      {
-        std::string name = TREE_STRING_POINTER (TREE_VALUE(TREE_VALUE(qq)));
-        if(!name.empty())
-        {
-          if(name.find(attrNo) != string::npos)
-          {
+      while (qq) {
+        string name = TREE_STRING_POINTER(TREE_VALUE(TREE_VALUE(qq)));
+
+        if (!name.empty()) {
+          if (name.find(attrNo) != string::npos) {
             return false;
           }
 
-          if(name.find(attr) != string::npos)
-          {
+          if (name.find(attr) != string::npos) {
             return true;
           }
-            
         }
+
         qq = TREE_CHAIN (qq);
       }
     }
   }
+
   return flag;
 }
